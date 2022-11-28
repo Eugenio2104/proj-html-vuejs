f386aaa3eec68fa7bd1537094caf4bdf7750ac79
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-28 14:19:02 +0100

    creazione welcome.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,26 +1,16 @@
 <script>
+import welcome from "./addcomponents/welcome.vue";
+
 export default {
   name: "AppMain",
+  components: {
+    welcome,
+  },
 };
 </script>
 
 <template>
-  <main>
-    <div class="welcome">
-      <div class="container">
-        <h1>welcome to fable</h1>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="container-gray">
-          <div class="elements"></div>
-          <div class="carousel-thumb"></div>
-        </div>
-      </div>
-    </div>
-  </main>
+  <welcome />
 </template>
 
 
---
+++ src/components/addcomponents/welcome.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "welcome",
+};
+</script>
+
+<template>
+  <div class="welcome-class">
+    <div class="container">
+      <h1>welcome to fable</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="container-gray">
+        <div class="elements"></div>
+        <div class="carousel-thumb"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../style/partial/variable" as *;
+@use "../style/partial/mixin" as *;
+
+.welcome-class {
+  .container {
+    @include centralFlex;
+    flex-direction: column;
+  }
+}
+</style>
